7fa34115b63c4e6a5b0317216f949608ccaf20e8
RutamBhagat <rutambhagat@gmail.com>
2024-05-25 23:09:51 +0530

    add GraphState class

---
+++ app/graph/state.py
@@ -0,0 +1,16 @@
+from typing import List, TypedDict
+
+
+class GraphState(TypedDict):
+    """Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        is_web_search_needed: whether to add search
+        documents: list of strings"""
+
+    question: str
+    generation: str
+    is_web_search_needed: bool
+    documents: List[str]
