2a7d5c46b2239f5ce51c5754643d62bf3d672b8c
RutamBhagat <rutambhagat@gmail.com>
2024-05-25 20:48:08 +0530

    first

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+if __name__ == "__main__":
+    print("Hello CRAG")
---
+++ app/server.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from dotenv import load_env, find_env
+
+_ = load_env(find_env())
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+# Edit this to add the chain you want to add
+add_routes(app, NotImplemented)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
