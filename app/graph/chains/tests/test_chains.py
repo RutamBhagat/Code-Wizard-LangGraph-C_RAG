13690b0bd0721dfb61a8f25b98f457d344cac1b3
RutamBhagat <rutambhagat@gmail.com>
2024-05-26 21:31:08 +0530

    remove retriever from test

--- app/graph/chains/tests/test_chains.py
+++ app/graph/chains/tests/test_chains.py
@@ -1,24 +1,12 @@
 from pprint import pprint
 from dotenv import load_dotenv, find_dotenv
 
-from pinecone import Pinecone
-from langchain_pinecone import PineconeVectorStore
-from langchain_openai import OpenAIEmbeddings
-
 from app.graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from app.graph.chains.generation import generation_chain
-from app.graph.consts import INDEX_NAME
+from app.ingestion import retriever
 
 
 _ = load_dotenv(find_dotenv())
-# Do not import this from retrieval_grader.py its giving an error because of the way you test in vscode as opposed to pycharm
-pc = Pinecone(environment="northamerica-northeast1-gcp")
-embeddings = OpenAIEmbeddings(disallowed_special=set())
-docsearch = PineconeVectorStore.from_existing_index(
-    index_name=INDEX_NAME, embedding=embeddings
-)
-
-retriever = docsearch.as_retriever()
 
 
 def test_retrival_grader_answer_yes() -> None:
--- app/ingestion.py
+++ app/ingestion.py
@@ -14,30 +14,29 @@ from app.graph.consts import INDEX_NAME
 
 _ = load_dotenv(find_dotenv())
 pc = Pinecone(environment="northamerica-northeast1-gcp")
-
-current_dir = os.getcwd()
-docs_path = os.path.join(
-    current_dir,
-    "..",
-    "langchain_docs",
-)
-
-md_elements = []
-for filename in os.listdir(docs_path):
-    if filename.endswith(".md") or filename.endswith(".mdx"):
-        file_path = os.path.join(docs_path, filename)
-        md_elements.extend(partition_md(filename=file_path))
-
-elements = chunk_by_title(md_elements)
-
-documents = []
-for element in elements:
-    metadata = element.metadata.to_dict()
-    del metadata["languages"]
-    metadata["source"] = metadata["filename"]
-    documents.append(Document(page_content=element.text, metadata=metadata))
-
 embeddings = OpenAIEmbeddings(disallowed_special=set())
+
+# current_dir = os.getcwd()
+# docs_path = os.path.join(
+#     current_dir,
+#     "..",
+#     "langchain_docs",
+# )
+
+# md_elements = []
+# for filename in os.listdir(docs_path):
+#     if filename.endswith(".md") or filename.endswith(".mdx"):
+#         file_path = os.path.join(docs_path, filename)
+#         md_elements.extend(partition_md(filename=file_path))
+
+# elements = chunk_by_title(md_elements)
+
+# documents = []
+# for element in elements:
+#     metadata = element.metadata.to_dict()
+#     del metadata["languages"]
+#     metadata["source"] = metadata["filename"]
+#     documents.append(Document(page_content=element.text, metadata=metadata))
 
 # Indexing the documents to Pinecone
 # print(f"Going to insert {len(documents)} Documents to Pinecone index {INDEX_NAME}")
