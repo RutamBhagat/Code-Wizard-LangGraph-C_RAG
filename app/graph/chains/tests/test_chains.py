0324fdba51a2ac60dd322140c8ffe1fec943a0ca
RutamBhagat <rutambhagat@gmail.com>
2024-05-26 15:49:37 +0530

    add generation chain

--- app/graph/chains/tests/test_chains.py
+++ app/graph/chains/tests/test_chains.py
@@ -1,7 +1,10 @@
+from pprint import pprint
 from dotenv import load_dotenv, find_dotenv
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from app.graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from app.graph.chains.generation import generation_chain
+
 
 _ = load_dotenv(find_dotenv())
 # Do not import this from retrieval_grader.py its giving an error because of the way you test in vscode as opposed to pycharm
@@ -30,3 +33,10 @@ def test_retrival_grader_answer_no() -> None:
         {"question": question, "document": doc_text}
     )
     assert res.is_document_relevant == "no"
+
+
+def test_generation_chain() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    pprint(generation)
