e2df1547ada23a3b1ce2a96dfc973edd4a70a659
RutamBhagat <rutambhagat@gmail.com>
2024-05-26 15:53:16 +0530

    add generation node

---
+++ app/graph/nodes/generate.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict
+from app.graph.chains.generation import generation_chain
+from app.graph.state import GraphState
+
+
+def generate(state: GraphState) -> Dict[str, Any]:
+    print("Generating answer...")
+    question = state["question"]
+    documents = state["documents"]
+
+    generation = generation_chain.invoke({"context": documents, "question": question})
+    return {"documents": documents, "generation": generation, "question": question}
