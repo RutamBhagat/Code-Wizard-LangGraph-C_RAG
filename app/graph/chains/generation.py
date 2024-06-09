4a2ee29b9e1cd4ce0c696fe2bfc1401b12a349a3
RutamBhagat <rutambhagat@gmail.com>
2024-06-09 17:39:43 +0530

    update generation.py

--- app/graph/chains/generation.py
+++ app/graph/chains/generation.py
@@ -1,9 +1,9 @@
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(temperature=0)
-prompt = """You are an assistant for question-answering tasks. 
+template = """You are an assistant for question-answering tasks. 
 Use the following pieces of retrieved context and the chat history to answer the question. 
 If you don't know the answer based on the provided information, just say that you don't know. 
 Use three sentences maximum and keep the answer concise unless the question asks for more details.
@@ -12,5 +12,11 @@ Question: {question}
 Chat History: {chat_history}
 Context: {context}
 Answer:"""
-prompt = PromptTemplate.from_template(prompt)
-generation_chain = prompt | llm | StrOutputParser()
+ANSWER_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{question}"),
+    ]
+)
+generation_chain = ANSWER_PROMPT | llm | StrOutputParser()
