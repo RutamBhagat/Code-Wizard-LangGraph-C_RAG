e3175915cb2f97b4ed57906cc481ca9cd3a49a0f
RutamBhagat <rutambhagat@gmail.com>
2024-06-17 02:16:24 +0530

    add cors

--- app/server.py
+++ app/server.py
@@ -1,31 +1,34 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv, find_dotenv
-from langserve import add_routes
+from starlette.middleware.cors import CORSMiddleware
 from app.api import router as api_router
-from app.graph.graph import c_rag_app
-from app.middleware import cors_middleware
 
 _ = load_dotenv(find_dotenv())
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-app.add_middleware(cors_middleware.middleware())
-
-app.include_router(api_router)
 
 @app.get("/health_check")
 async def health_check():
     return {"status": "ok"}
 
 
-# Edit this to add the chain you want to add
-add_routes(app, c_rag_app, path="/code_wizard")
+app.include_router(api_router)
+
 
 if __name__ == "__main__":
     import uvicorn
