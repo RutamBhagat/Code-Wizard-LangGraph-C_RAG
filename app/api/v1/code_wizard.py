689816dedd0591afadd7f8343fc0ceb5b1a65830
RutamBhagat <rutambhagat@gmail.com>
2024-11-03 14:05:05 -0800

    feat(ui): remove api call

--- app/api/v1/code_wizard.py
+++ app/api/v1/code_wizard.py
@@ -22,7 +22,7 @@ async def code_wizard(request_body: RequestBody = Body(...)):
     question = request_body.question
     start_time = time.time()
     config = {"configurable": {"thread_id": request_body.chat_id}}
-    res = c_rag_app.invoke(input={"question": question}, config=config)
+    res = c_rag_app.stream(input={"question": question}, config=config)
     end_time = time.time()
     time_taken = end_time - start_time
     print(f"Time taken: {time_taken:.2f} seconds")
