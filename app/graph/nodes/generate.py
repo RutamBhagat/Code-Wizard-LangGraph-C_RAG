bc61f14d86aafe21b68ca016007c294281967dd7
RutamBhagat <rutambhagat@gmail.com>
2024-11-06 00:23:14 -0800

    feat(retrieve): fix chat history bug

--- app/graph/nodes/generate.py
+++ app/graph/nodes/generate.py
@@ -1,17 +1,18 @@
 from typing import Any, Dict
+
+from langchain.schema import AIMessage, HumanMessage
 from app.graph.chains.generation import generation_chain
 from app.graph.state import GraphState
 
 
 def generate(state: GraphState) -> Dict[str, Any]:
-    documents = state.documents
-    chat_history = state.chat_history
     state.generation = generation_chain.invoke(
         {
-            "context": documents,
+            "context": state.documents,
             "question": state.enhanced_query,
-            "chat_history": chat_history or [],
+            "chat_history": state.chat_history or [],
         }
     )
-    state.chat_history = state.generation
+    state.chat_history = HumanMessage(content=state.question)
+    state.chat_history = AIMessage(content=state.generation)
     return state
