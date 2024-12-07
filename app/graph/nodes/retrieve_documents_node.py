615dacdc851c6c8b289009c508b7c800b7360773
RutamBhagat <rutambhagat@gmail.com>
2024-12-07 05:58:11 -0800

    refactor(graph): remove time_it decorator from nodes and delete utility module

--- app/graph/nodes/retrieve_documents_node.py
+++ app/graph/nodes/retrieve_documents_node.py
@@ -1,10 +1,8 @@
 from app.graph.nodes.generate_enhanced_query_node import generate_enhanced_query_node
 from app.graph.state import GraphState
-from app.graph.utils.time import time_it
 from app.ingestion import retriever
 
 
-@time_it
 def retrieve_documents_node(state: GraphState) -> GraphState:
     """Node for retrieving documents using the enhanced query"""
     new_docs = retriever.invoke(state.enhanced_query)
