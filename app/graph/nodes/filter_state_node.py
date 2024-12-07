b9f8e986326609e7821fc8f2c656ca70e57c7ee5
RutamBhagat <rutambhagat@gmail.com>
2024-12-07 09:03:00 -0800

    refactor(graph): update chat_history management in filter_state_node to trim messages and limit token usage

--- app/graph/nodes/filter_state_node.py
+++ app/graph/nodes/filter_state_node.py
@@ -1,3 +1,5 @@
+from langchain_core.messages import trim_messages
+from langchain_openai import ChatOpenAI
 from app.graph.state import GraphState
 from app.graph.utils.time import track_execution_time
 
@@ -5,9 +7,16 @@ from app.graph.utils.time import track_execution_time
 @track_execution_time
 def filter_state_node(state: GraphState):
     state.execution_times.clear()
+    messages = trim_messages(
+        messages=state.chat_history,
+        max_tokens=1000,
+        token_counter=ChatOpenAI(model="gpt-4o-mini"),
+        strategy="last",
+        allow_partial=False,
+    )
     return {
         "documents": [],
         "generation": "",
         "execution_times": state.execution_times,
-        "chat_history": state.chat_history[-6:],
+        "chat_history": messages,
     }
