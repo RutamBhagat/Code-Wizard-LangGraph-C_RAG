36e814c4688d2fab2aec5442234dc3e5bfca1056
RutamBhagat <rutambhagat@gmail.com>
2024-12-07 09:23:07 -0800

    refactor(graph): enhance message trimming logic in filter_state_node with constants for max tokens and model

--- app/graph/nodes/filter_state_node.py
+++ app/graph/nodes/filter_state_node.py
@@ -6,14 +6,23 @@ from app.graph.utils.time import track_execution_time
 
 @track_execution_time
 def filter_state_node(state: GraphState):
+    MAX_TOKENS = 1000
+    MODEL = "gpt-4o-mini"
+
     state.execution_times.clear()
-    messages = trim_messages(
-        messages=state.chat_history,
-        max_tokens=1000,
-        token_counter=ChatOpenAI(model="gpt-4o-mini"),
-        strategy="last",
-        allow_partial=False,
+
+    messages = (
+        trim_messages(
+            messages=state.chat_history,
+            max_tokens=MAX_TOKENS,
+            token_counter=ChatOpenAI(model=MODEL),
+            strategy="last",
+            allow_partial=False,
+        )
+        if len(state.chat_history) > 2
+        else state.chat_history
     )
+
     return {
         "documents": [],
         "generation": "",
